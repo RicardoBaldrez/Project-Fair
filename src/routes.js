7c364c5836f8c1b98691b42ae1baaf0526ff1cb8
root <root@DESKTOP-H3T39UL.localdomain>
2021-10-24 21:51:34 -0300

    Separating Responsibilities from Routes and User Context

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,18 @@
-import { useState } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Login from 'pages/Login';
 import Feira from 'pages/Feira';
 import Carrinho from 'pages/Carrinho';
-import { UserContext } from 'common/context/User';
+import { UserProvider } from 'common/context/User';
 
 export default function Router() {
-  const [name, setName] = useState("");
-  const [balance, setBalance] = useState(0);
-
   return(
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          {/* Provendo os valores para os componentes encapsulados */}
-          <UserContext.Provider value={{ name, setName, balance, setBalance }}>
+          <UserProvider>
             <Login />
-          </UserContext.Provider>
+          </UserProvider>
         </Route>
         <Route path="/fair">
           <Feira />
